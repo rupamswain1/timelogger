660715aa4342a960730451e364aa1ae47b90c651
rupamswain1 <rupamswain1@gmail.com>
2021-12-24 00:34:48 +0530

    Updates

---
+++ precache-manifest.dc1b2db05fec0e26283660468b7c0638.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a6dfcda89602504d6cfb73ec43ae18",
+    "url": "/timelogger/index.html"
+  },
+  {
+    "revision": "6962fb4365162f51864d",
+    "url": "/timelogger/static/css/2.61061fbe.chunk.css"
+  },
+  {
+    "revision": "c3866205b511ee33b8ad",
+    "url": "/timelogger/static/css/main.41fb6482.chunk.css"
+  },
+  {
+    "revision": "6962fb4365162f51864d",
+    "url": "/timelogger/static/js/2.fbfab578.chunk.js"
+  },
+  {
+    "revision": "7402969f196092587bc95d58e8da596c",
+    "url": "/timelogger/static/js/2.fbfab578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd5e1de02135af811f2",
+    "url": "/timelogger/static/js/3.4f070355.chunk.js"
+  },
+  {
+    "revision": "c3866205b511ee33b8ad",
+    "url": "/timelogger/static/js/main.ca1f154d.chunk.js"
+  },
+  {
+    "revision": "e02e9eb62450f673ae96",
+    "url": "/timelogger/static/js/runtime-main.be6a9263.js"
+  }
+]);
